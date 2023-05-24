96df5f265e1c036fa337afdb5d1502618d290432
vsbitcoding <vs.bitcoding@gmail.com>
2023-05-24 12:38:24 +0530

    --add singal data load fountionality

--- contentgbp/app/views.py
+++ contentgbp/app/views.py
@@ -9,38 +9,43 @@ from rest_framework.response import Response
 from .serializers import YourModelSerializer
 from .models import YourModel
 
-@login_required(login_url='login')
+
+@login_required(login_url="login")
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            return redirect("login")
     else:
         form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, "register.html", {"form": form})
+
 
 def login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect('home')
+            return redirect("home")
     else:
         form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
+    return render(request, "login.html", {"form": form})
 
-@login_required(login_url='login')
+
+@login_required(login_url="login")
 def logout(request):
     auth_logout(request)
-    return redirect('login')
+    return redirect("login")
+
 
 @login_required
 def postContent_tool(request):
-    return render(request, 'PostContentTool.html')
+    return render(request, "PostContentTool.html")
 
 
 class FileUploadAPIView(APIView):
@@ -50,76 +55,110 @@ class FileUploadAPIView(APIView):
         return Response(serializer.data)
 
     def post(self, request, format=None):
-        file_obj = request.FILES.get('file')
+        if "file" in request.FILES:
+            # File upload case
+            file_obj = request.FILES.get("file")
 
-        if not file_obj:
-            return Response({'error': 'No file provided.'}, status=400)
+            if not file_obj:
+                return Response({"error": "No file provided."}, status=400)
 
-        try:
-            if file_obj.name.endswith('.csv'):
-                df = pd.read_csv(file_obj)
-            elif file_obj.name.endswith('.xlsx'):
-                df = pd.read_excel(file_obj)
-            else:
-                return Response({'error': 'Unsupported file format.'}, status=400)
+            try:
+                if file_obj.name.endswith(".csv"):
+                    df = pd.read_csv(file_obj)
+                elif file_obj.name.endswith(".xlsx"):
+                    df = pd.read_excel(file_obj)
+                else:
+                    return Response({"error": "Unsupported file format."}, status=400)
 
-            YourModel.objects.all().update(flag=False)  # Set flag=False for all existing objects
+                YourModel.objects.all().update(
+                    flag=False
+                )  # Set flag=False for all existing objects
 
-            for _, row in df.iterrows():
-                obj = YourModel(
-                    company_name=row['Company Name'],
-                    character_long=row['character Long'],
-                    category=row['Category'],
-                    keywords=row['Keywords'],
-                    city=row['City'],
-                    tech_name=row['Tech Name'],
-                    stars=row['Stars'],
-                    review_writing_style=row['Review writing Style'],
-                    flag=True  # Set flag=True for new object
+                for _, row in df.iterrows():
+                    obj = YourModel.objects.create(
+                        company_name=row["Company Name"],
+                        character_long=row["character Long"],
+                        category=row["Category"],
+                        keywords=row["Keywords"],
+                        city=row["City"],
+                        tech_name=row["Tech Name"],
+                        stars=row["Stars"],
+                        review_writing_style=row["Review writing Style"],
+                        flag=True,  # Set flag=True for new object
+                    )
+
+                    # Call ChatGPT API
+                    response = self.call_chatgpt_api(obj)
+
+                    # Save response in content field
+                    obj.content = response["content"]
+                    obj.save()
+
+                return Response({"message": "Data uploaded successfully."}, status=201)
+            except Exception as e:
+                return Response({"error": str(e)}, status=400)
+        else:
+            # Individual data upload case
+            try:
+                company_name = request.data.get("company_name")
+                character_long = request.data.get("character_long")
+                category = request.data.get("category")
+                keywords = request.data.get("keywords")
+                city = request.data.get("city")
+                tech_name = request.data.get("tech_name")
+                stars = request.data.get("stars")
+                review_writing_style = request.data.get("review_writing_style")
+
+                obj = YourModel.objects.create(
+                    company_name=company_name,
+                    character_long=character_long,
+                    category=category,
+                    keywords=keywords,
+                    city=city,
+                    tech_name=tech_name,
+                    stars=stars,
+                    review_writing_style=review_writing_style,
+                    flag=True,  # Set flag=True for new object
                 )
-                obj.save()
 
                 # Call ChatGPT API
                 response = self.call_chatgpt_api(obj)
 
                 # Save response in content field
-                obj.content = response['content']
+                obj.content = response["content"]
                 obj.save()
 
-            return Response({'message': 'Data uploaded successfully.'}, status=201)
-        except Exception as e:
-            return Response({'error': str(e)}, status=400)
+                return Response({"message": "Data uploaded successfully."}, status=201)
+            except Exception as e:
+                return Response({"error": str(e)}, status=400)
 
     def call_chatgpt_api(self, obj):
-        api_key = 'YOUR_API_KEY'  # Replace with your ChatGPT API key
-        url = 'https://api.openai.com/v1/chat/completions'
+        api_key = "YOUR_API_KEY"  # Replace with your ChatGPT API key
+        url = "https://api.openai.com/v1/chat/completions"
 
         headers = {
-            'Authorization': f'Bearer {api_key}',
-            'Content-Type': 'application/json'
+            "Authorization": f"Bearer {api_key}",
+            "Content-Type": "application/json",
         }
 
         data = {
-            'messages': [
+            "messages": [
                 {
-                    'role': 'user',
-                    'content': f"please write me a review for {obj.company_name} company\n"
-                               f"Character Long: {obj.character_long}\n"
-                               f"Category: {obj.category}\n"
-                               f"Keywords: {obj.keywords}\n"
-                               f"City: {obj.city}\n"
-                               f"Tech Name: {obj.tech_name}\n"
-                               f"Stars: {obj.stars}\n"
-                               f"Review Writing Style: {obj.review_writing_style}"
+                    "role": "user",
+                    "content": f"please write me a review for {obj.company_name} company\n"
+                    f"Character Long: {obj.character_long}\n"
+                    f"Category: {obj.category}\n"
+                    f"Keywords: {obj.keywords}\n"
+                    f"City: {obj.city}\n"
+                    f"Tech Name: {obj.tech_name}\n"
+                    f"Stars: {obj.stars}\n"
+                    f"Review Writing Style: {obj.review_writing_style}",
                 }
             ],
-            'max_tokens': 50
+            "max_tokens": 50,
         }
 
         response = requests.post(url, json=data, headers=headers)
         response.raise_for_status()
 
         return response.json()
-
-
-
