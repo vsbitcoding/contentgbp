a4b85538978de89f1f65d978842faeef3a2b4551
vsbitcoding <vs.bitcoding@gmail.com>
2023-05-26 11:42:33 +0530

    --.

--- contentgbp/contentgbp/settings.py
+++ contentgbp/contentgbp/settings.py
@@ -149,8 +149,8 @@ else:
 
 
 # Celery Configuration
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://localhost:6380/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6380/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
